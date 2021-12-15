bd380a3f4fa0df4465a8053e2ec7ed6ee9535732
Stanislau Sialiunin <infleshlightwetrust@gmail.com>
2021-12-15 13:05:02 +0300

    bugxifes

--- functional-mini-library.js
+++ functional-mini-library.js
@@ -1,97 +1,97 @@
 function isArray(obj) {
-    return obj instanceof Array
-}
+    return obj instanceof Array;
+};
 
 function isDate(obj) {
-    return obj instanceof Date
-}
+    return obj instanceof Date;
+};
 
 function isBoolean(obj) {
-    return typeof obj === 'boolean'
-}
+    return typeof obj === 'boolean';
+};
 
 function isNumber(obj) {
-    return typeof obj === 'number'
-}
+    return typeof obj === 'number';
+};
 
 function isString(obj) {
-    return typeof obj === 'string'
-}
+    return typeof obj === 'string';
+};
 
 function isFunction(obj) {
-    return typeof obj === 'function'
-}
+    return typeof obj === 'function';
+};
 
 function isUndefined(obj) {
-    return typeof obj === 'undefined'
-}
+    return typeof obj === 'undefined';
+};
 
 function isNull(obj) {
-    return obj === null
-}
+    return obj === null;
+};
 
 // Working with arrays:
 
 function first(arr) {
     if (!isArray(arr)) {
-        return null
-    }
+        return null;
+    };
 
-    return arr[0]
-}
+    return arr[0];
+};
 
 function last(arr) {
     if (!isArray(arr)) {
-        return null
-    }
+        return null;
+    };
 
-    return arr[arr.length - 1]
-}
+    return arr[arr.length - 1];
+};
 
 function take(arr, number) {
     if (!isArray(arr)) {
-        return null
-    }
+        return null;
+    };
 
     if (!isNumber(number)) {
-        return `${number} Is not a number`
-    }
+        return `${number} Is not a number`;
+    };
 
-    const newArr = []
+    const newArr = [];
     for (let i = 0; i < number; i++) {
-        newArr.push(arr[i])
-    }
+        newArr.push(arr[i]);
+    };
 
-    return newArr
-}
+    return newArr;
+};
 
 function skip(arr, number) {
     if (!isArray(arr)) {
-        return null
-    }
+        return null;
+    };
 
     if (!isNumber(number)) {
-        return `${number} Is not a number`
-    }
+        return `${number} Is not a number`;
+    };
 
-    const newArr = []
+    const newArr = [];
     for (let i = number; i < arr.length; i++) {
-        newArr.push(arr[i])
-    }
+        newArr.push(arr[i]);
+    };
 
-    return newArr
-}
+    return newArr;
+};
 
 function asChain(arr) {
     class ChainArray extends Array {
         skip(n) {
-            return asChain(skip(arr, n))
-        }
+            return asChain(skip(arr, n));
+        };
 
         take(n) {
-            return asChain(take(arr, n))
-        }
-    }
+            return asChain(take(arr, n));
+        };
+    };
 
-    return new ChainArray(...arr)
-}
+    return new ChainArray(...arr);
+};
